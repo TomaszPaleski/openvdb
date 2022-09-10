708ccc2bc5fb7777be5bc99c9448ad89b61f2df5
Tomasz Paleski <tomasz.paleski@object.pl>
2022-09-10 19:01:21 +0200

    build

---
+++ zlib-1.2.12/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/PC/source/repos/openvdb/zlib-1.2.12
+# Build directory: C:/Users/PC/source/repos/openvdb/zlib-1.2.12/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/PC/source/repos/openvdb/zlib-1.2.12/CMakeLists.txt;235;add_test;C:/Users/PC/source/repos/openvdb/zlib-1.2.12/CMakeLists.txt;0;")
